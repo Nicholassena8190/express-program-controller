e7b4d762ce5efa0eb6431f4c70ed99a77a8df3d9
Nicsena <36643037+Nicsena@users.noreply.github.com>
2020-05-12 22:59:37 -0700

    v1.0.0b Release - Mini Bug Fix - One Bug (NaN Bug)

--- server.js
+++ server.js
@@ -40,16 +40,17 @@ app.get('/forcestop/*', (req, res) => {
 app.get('/forcestop', (req, res) => {
 
     if (req.url === "/forcestop" || req.url === "/forcestop?" || req.url === "/forcestop?program" || req.url === "/forcestop?program=") {
-    res.send("No or Invaild Params. <b></b>To force stop a program, enter in your url bar: " +  +"/forcestop?program=<PROGRAM_NAME_HERE>")
+    res.send("No or Invaild Params. <b></b>To force stop a program, enter in your url bar: /forcestop?program=<PROGRAM_NAME_HERE>")
 
     } else {
 
             //csrss.exe is a critical system process, if killed, it will bsod.
             //ntoskrnl.exe is Windows NT Kernel, a critical system process - if killed, it will bsod.
             //SYSTEM is a critical system process that windows won't let you kill, but it's added anyways.
-        if (req.url === "/stop?program=csrss.exe" || req.url === "/stop?program=ntoskrnl.exe" || req.url === "/stop?program=winlogon.exe" || req.url === "/stop?program=wininit.exe" || req.url === "/stop?program=smss.exe" || req.url === "/stop?program=PUT_PROGRAM_NAME") { // Bad Basic Protection - People can bypass this.
-            res.send("You cannot kill this process.")
-        
+            //svchost.exe is maybe a critical one.
+            //cmd.exe is a normal process that can be killed.
+        if (req.url === "/stop?program=csrss.exe" || req.url === "/stop?program=ntoskrnl.exe" || req.url === "/stop?program=winlogon.exe" || req.url === "/stop?program=wininit.exe" || req.url === "/stop?program=smss.exe" || req.url === "/stop?program=cmd.exe" || req.url === "/stop?program=svchost.exe" || req.url === "/stop?program=PUT_PROGRAM_NAME") { // Bad Basic Protection - People can bypass this.
+            res.send("You cannot kill this process/program.")
             } else
 
     exec("taskkill /f /im " +req.query.program, (error, stdout, stderr) => {  //For this to work, force stopping a program requires administrator permissions.
@@ -76,7 +77,7 @@ app.get('/forcestop', (req, res) => {
 app.get('/stop', (req, res) => {
 
     if (req.url === "/stop" || req.url === "/stop?" || req.url === "/stop?program" || req.url === "/stop?program=") {
-    res.send("No or Invaild Params. <b></b>To stop a program, enter in your url bar: " + + "/stop?program=<PROGRAM_NAME_HERE>")
+    res.send("No or Invaild Params. <b></b>To stop a program, enter in your url bar: /stop?program=<PROGRAM_NAME_HERE>")
     
     } else {
 
@@ -109,7 +110,7 @@ app.get('/stop', (req, res) => {
 app.get('/run', (req, res) => {
 
 if (req.url === "/run" || req.url === "/run?" || req.url === "/run?program" || req.url === "/run?program=") {
-res.send("No or Invaild Params. <b></b>To run a program, enter in your url bar: " + + "/run?program=<PROGRAM_NAME_HERE>")
+res.send("No or Invaild Params. <b></b>To run a program, enter in your url bar: /run?program=<PROGRAM_NAME_HERE>")
 
 } else {
 
